d7b783d2b705b2bc4eed94bb909aeb909981aeb0
ShubhamVsCode <shubhamvscode@gmail.com>
2022-08-16 20:35:10 +0530

    Working fine

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,25 +2,22 @@ const User = require("../models/user");
 const BigPromise = require("../middlewares/bigPromise");
 const CustomError = require("../utils/customError");
 const cookieToken = require("../utils/cookieToken");
-const fileUpload = require("express-fileupload");
 const cloudinary = require("cloudinary");
+const crypto = require("crypto");
 
 exports.signup = BigPromise(async (req, res, next) => {
-  // let result;
-
+  //let result;
   if (!req.files) {
     return next(new CustomError("photo is required for signup", 400));
   }
 
   const { name, email, password } = req.body;
 
-  if (!name || !email || !password) {
-    return next(
-      new CustomError("Name, Email and Password must be provided", 400)
-    );
+  if (!email || !name || !password) {
+    return next(new CustomError("Name, email and password are required", 400));
   }
 
-  let file = req.files.photo; // call photo on frontend
+  let file = req.files.photo;
 
   const result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
     folder: "users",
@@ -38,8 +35,35 @@ exports.signup = BigPromise(async (req, res, next) => {
     },
   });
 
-  user = JSON.stringify(user);
-  console.log(user);
+  cookieToken(user, res);
+});
 
+exports.login = BigPromise(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // check presence of email and password
+  if (!email || !password) {
+    return next(new CustomError("Please enter your email and password", 400));
+  }
+
+  // get user from DB
+  const user = await User.findOne({ email }).select("+password");
+
+  // check if user exists in DB
+  if (!user) {
+    return next(
+      new CustomError("Email or password does not match or exists", 400)
+    );
+  }
+
+  // check if password is correct
+  const isPasswordCorrect = await user.isValidatedPassword(password);
+
+  // if password don't match
+  if (!isPasswordCorrect) {
+    return next(new CustomError("Password is incorrect", 400));
+  }
+
+  // if all good we send the token
   cookieToken(user, res);
 });
